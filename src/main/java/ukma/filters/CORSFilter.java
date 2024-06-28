b4dd21a3a58bba21b7d9d7c94b0e1b206cd5e29d
MishaTarasenko <127847522+MishaTarasenko@users.noreply.github.com>
2024-06-28 13:34:07 +0300

    do some changes

--- src/main/java/ukma/filters/CORSFilter.java
+++ src/main/java/ukma/filters/CORSFilter.java
@@ -8,19 +8,17 @@ import java.io.IOException;
 public class CORSFilter extends Filter {
     @Override
     public void doFilter(HttpExchange exchange, Chain chain) throws IOException {
-        // Set CORS headers
+
         exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
         exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
         exchange.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
 
-        // If the request method is OPTIONS, respond with 204 and return
         if ("OPTIONS".equalsIgnoreCase(exchange.getRequestMethod())) {
             exchange.sendResponseHeaders(204, -1);
             return;
         }
 
-        // Continue with the next handler in the chain
         chain.doFilter(exchange);
     }
 
